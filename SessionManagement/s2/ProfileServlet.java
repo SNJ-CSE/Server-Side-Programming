de7b8a843067395b412fddfa72a63c9711697534
S N Jaiswal <70748777+SNJ-CSE@users.noreply.github.com>
2022-08-26 11:07:14 +0530

    Add files via upload

---
+++ SessionManagement/s2/ProfileServlet.java
@@ -0,0 +1,59 @@
+package s2;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class ProfileServlet
+ */
+@WebServlet("/ProfileServlet")
+public class ProfileServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ProfileServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		 response.setContentType("text/html");  
+	        PrintWriter out=response.getWriter();  
+	        request.getRequestDispatcher("Link.html").include(request, response);  
+	          
+	        HttpSession session=request.getSession(false);  
+	        if(session!=null){  
+	        String name=(String)session.getAttribute("name");  
+	          
+	        out.print("Hello, "+name+" Welcome to Profile");  
+	        }  
+	        else{  
+	            out.print("Please login first");  
+	            request.getRequestDispatcher("Login.html").include(request, response);  
+	        }  
+	        out.close();  
+	    }  
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
